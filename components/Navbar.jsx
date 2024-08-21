5e125bff15b28c80f5bb922dfb258e1891325fd4
Roaa K <ruaakhuldoon.26@gmail.com>
2024-08-21 16:31:27 +0300

    ADD NAVBAR.JSX

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from "react"
 export default function Navbar()
 {
     return(
-        <>
-        </>
+        <nav> 
+         <img src='./images/Globe.png' />
+         <span className="nav--text">my travel journal.</span>
+        </nav>
     )
 }
